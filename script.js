122a6fd4d5bb832a5abaca7e986f4e21112f14c1
Ryan <69058586+ryan-schaefer@users.noreply.github.com>
2025-07-16 15:34:28 -0700

    Update script.js
    Added stuff to automate lazy loading

--- script.js
+++ script.js
@@ -20,6 +20,37 @@ document.querySelectorAll('.carousel-container').forEach(container => {
   });
 });
 
+document.querySelectorAll('.video').forEach(div => {
+  const id = div.dataset.id;
+  const width = div.dataset.width;
+  const height = div.dataset.height;
+  const img = document.createElement('img');
+  img.src = `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+  img.width = width;
+  img.height = height;
+  img.alt = div.dataset.desc;
+  img.style.cursor = 'pointer';
+
+  div.appendChild(img);
+
+  img.addEventListener('click', () => {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${id}`;
+    iframe.width = width;
+    iframe.height = height;
+    iframe.frameBorder = '20';
+    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+    iframe.referrerPolicy = 'strict-origin-when-cross-origin';
+    iframe.allowFullscreen = true;
+    iframe.loading = 'lazy'; // Add lazy loading to the iframe
+
+    div.innerHTML = ''; // Clear the div
+    div.appendChild(iframe); // Add the iframe
+  }
+});
+
+
+
 // Automatically add loading="lazy" to all iframes in carousels
 document.querySelectorAll('.carousel').forEach(iframe => {
     iframe.setAttribute('loading', 'lazy');
